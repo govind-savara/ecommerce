b3803f1cf638fff0c684354f7f6ddcbed152b994
govind-savara <govind.savara001@gmail.com>
2024-05-21 17:09:20 +0530

    Added crud APIs for review

--- product_app/urls.py
+++ product_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ProductList, ProductDetail
+from .views import ProductList, ProductDetail, ReviewList, ReviewDetails
 
 urlpatterns = [
     path('products/', ProductList.as_view()),
     path('products/<int:product_id>/', ProductDetail.as_view()),
+    path('product_reviews/', ReviewList.as_view()),
+    path('product_reviews/<str:review_id>/', ReviewDetails.as_view())
 ]
--- product_app/views.py
+++ product_app/views.py
@@ -1,7 +1,9 @@
+from bson.objectid import ObjectId
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
+from user_app.models import User
 from utility.common_utils import parse_mongo_json
 from .models import (
     product_collection, get_default_product_dict, get_updated_product_data,
@@ -13,12 +15,12 @@ from .models import (
 
 class ProductList(APIView):
     """
-    List all snippets, or create a new snippet.
+    List all products, or create a new product.
     """
 
     def get(self, request):
-        products_data = product_collection.get_records()
-        return Response(products_data)
+        products_data = product_collection.find({})
+        return Response(parse_mongo_json(list(products_data)))
 
     def post(self, request):
         product_name = request.data.get("name")
@@ -72,3 +74,118 @@ class ProductDetail(APIView):
             return Response({"message": "Product deleted!"}, HTTP_200_OK)
         else:
             return Response({"error": "Unable to delete product!"}, HTTP_400_BAD_REQUEST)
+
+
+class ReviewList(APIView):
+    """
+    List all Review data or create a new review of product
+    """
+    def get(self, request):
+        query = {}
+
+        if "product_id" in request.data:
+            try:
+                query["product_id"] = int(request.data["product_id"])
+            except Exception as e:
+                return Response({"error": "Please provide a valid product id"}, HTTP_400_BAD_REQUEST)
+
+        if "user_id" in request.data:
+            try:
+                query["user_id"] = int(request.data["user_id"])
+            except Exception as e:
+                return Response({"error": "Please provide a valid user id"}, HTTP_400_BAD_REQUEST)
+
+        reviews = review_collection.find(query)
+        return Response(parse_mongo_json(list(reviews)), HTTP_200_OK)
+
+    def post(self, request):
+        product_id = request.data.get("product_id")
+        if not product_id:
+            return Response({"error": "Product id is missing!"}, HTTP_400_BAD_REQUEST)
+
+        try:
+            product_id = int(product_id)
+        except Exception as e:
+            return Response({"error": "Please provide a valid product id"}, HTTP_400_BAD_REQUEST)
+
+        product_data = product_collection.find_one({"product_id": product_id})
+        if not product_data:
+            return Response({"error": f"Product does not exist with product_id={product_id}"}, HTTP_400_BAD_REQUEST)
+
+        user_id = request.data.get("user_id")
+        if not user_id:
+            return Response({"error": "User id is missing!"}, HTTP_400_BAD_REQUEST)
+
+        user_data = User.objects.filter(id=user_id).first()
+        if not user_data:
+            return Response({"error": "User does not exists!"}, HTTP_400_BAD_REQUEST)
+
+        review_data = review_collection.find_one({"product_id": product_id, "user_id": user_data.id})
+        if review_data:
+            return Response({"error": "Already you reviewed this product."}, HTTP_400_BAD_REQUEST)
+
+        rating = request.data.get("rating")
+        if not rating:
+            return Response({"error": "Please provide rating between 1 to 5"})
+
+        try:
+            rating = int(rating)
+        except Exception as e:
+            return Response({"error": "Please provide a valid rating between 1 to 5"})
+
+        review_data = get_default_review_dict()
+        review_data.update({
+            "product_id": product_id,
+            "user_id": user_data.id,
+            "rating": rating,
+        })
+
+        if "comment" in request.data:
+            review_data["comment"] = request.data["comment"]
+
+        result = review_collection.insert_one(review_data)
+        print(f"review id: {result.inserted_id}")
+        return Response({"message": "Review created!"}, HTTP_201_CREATED)
+
+
+class ReviewDetails(APIView):
+    """
+    Retrieve, update or delete a review
+    """
+    def get(self, request, review_id):
+        query = {'_id': ObjectId(review_id)}
+        review_data = review_collection.find_one(query)
+        return Response(parse_mongo_json(review_data))
+
+    def put(self, request, review_id):
+        query = {"$_id": ObjectId(review_id)}
+        review_data = review_collection.find_one(query)
+        if not review_data:
+            return Response({"error": f"Review does not exist!"}, HTTP_400_BAD_REQUEST)
+
+        new_data = {}
+        if "rating" in request.data:
+            try:
+                new_data["rating"] = int(request.data["rating"])
+            except Exception as e:
+                return Response({"error": "Please provide a valid rating between 1 to 5"})
+
+        if "comment" in request.data:
+            new_data["comment"] = request.data["comment"]
+
+        if not new_data:
+            return Response({"error": "Please provide rating or comment to update"}, HTTP_400_BAD_REQUEST)
+
+        result = review_collection.update_one(query, {"$set": new_data})
+        if result.modified_count:
+            return Response({"message": "Review details updated!"}, HTTP_200_OK)
+        else:
+            return Response({"error": "Unable to update review details!"}, HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, review_id):
+        query = {"_id": ObjectId(review_id)}
+        result = review_collection.delete_one(query)
+        if result.deleted_count:
+            return Response({"message": "Review deleted!"}, HTTP_200_OK)
+        else:
+            return Response({"error": "Unable to delete review!"}, HTTP_400_BAD_REQUEST)
